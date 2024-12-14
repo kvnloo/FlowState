902c09afa1b6eac2f426fefdfa1f9e98f781e723
lesseradmin <veeman961@gmail.com>
2024-12-14 09:20:03 -0600

    Either Hot Shit or Total Bollocks

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -12,11 +12,11 @@ BACKEND_DIR="$ROOT_DIR/backend"
 # Navigate to the backend directory
 cd "$BACKEND_DIR"
 echo "Installing dependencies with pipenv..."
-pipenv install --dev
+pipenv install
 
 # Build the Sphinx documentation
 echo "Building the documentation with Sphinx..."
 pipenv run sphinx-build -b html "$DOCS_DIR" "$DOCS_DIR/_build/html"
 
 echo "Documentation successfully built!"
-open "$DOCS_DIR"/_build/html/index.html
+# open "$DOCS_DIR"/_build/html/index.html
